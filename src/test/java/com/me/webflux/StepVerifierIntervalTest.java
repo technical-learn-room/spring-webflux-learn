7285a6f7a0fcd91d49be784b94da7c6d6067af43
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-05-20 23:10:13 +0900

    add comments for interval test use virtual time.

---
+++ src/test/java/com/me/webflux/StepVerifierIntervalTest.java
@@ -0,0 +1,18 @@
+package com.me.webflux;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
+
+public class StepVerifierIntervalTest {
+
+    @Test
+    public void intervalTest() {
+        Flux<Long> flux = Flux.interval(Duration.ofSeconds(10)).take(10);
+        StepVerifier.create(flux)
+                .expectNextCount(10)
+                .verifyComplete();
+    }
+}
---
+++ src/test/java/com/me/webflux/StepVerifierVirtualTimeIntervalTest.java
@@ -0,0 +1,28 @@
+package com.me.webflux;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
+
+public class StepVerifierVirtualTimeIntervalTest {
+
+    @Test
+    public void virtualTimeIntervalTest() {
+        StepVerifier.withVirtualTime(() -> Flux.interval(Duration.ofSeconds(10)).take(10))
+                .thenAwait(Duration.ofSeconds(50))
+                .expectNextCount(5)
+                .thenAwait(Duration.ofSeconds(30))
+                .expectNextCount(3)
+                .thenAwait(Duration.ofSeconds(20))
+                .expectNextCount(2)
+                .verifyComplete();
+    }
+}
+
+/*
+실제로는 100초가 걸리는 작업이지만,
+withVirtualTime() 메소드를 이용해서 Virtual Time 을 통해 데이터 스트림을 처리하기 때문에
+훨씬 빠른 시간에 테스트 코드를 성공할 수 있었다.
+ */
