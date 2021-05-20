a7e59bda66305ac757bc39263a42d58134d9f4f7
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-05-20 20:12:14 +0900

    add comments for simple flux test.

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,9 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.projectreactor:reactor-test:3.4.5")
 }
 
 tasks.withType<KotlinCompile> {
---
+++ src/test/java/com/me/webflux/StepVerifierSimpleFluxTest.java
@@ -0,0 +1,19 @@
+package com.me.webflux;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+public class StepVerifierSimpleFluxTest {
+
+    @Test
+    public void simpleFluxTest() {
+        var flux = Flux.just("apple", "banana", "melon");   // 테스트할 데이터 스트림 생성
+        StepVerifier.create(flux)                                       // 테스트할 데이터 스트림으로 StepVerifier 생성
+                .expectNext("apple")                                    // 다음엔 'apple' 데이터 등장 검증
+                .expectNext("banana")                                   // 다음엔 'banana' 데이터 등장 검증
+                .expectNext("melon")                                    // 다음엔 'melon' 데이터 등장 검증
+                .expectComplete()                                       // 지금까지 모두 성공했는지 검증
+                .verify();                                              // 검증 끝
+    }
+}
