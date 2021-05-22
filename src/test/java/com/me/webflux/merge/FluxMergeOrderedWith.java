ca34f9d7cc92b360b944ad75b7b09e5ad94bf5e6
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-05-22 23:09:34 +0900

    add comments for How to merge Publihers.

--- src/test/java/com/me/webflux/merge/FluxMergeOrderedWith.java
+++ src/test/java/com/me/webflux/merge/FluxMergeOrderedWith.java
@@ -6,16 +6,17 @@ import reactor.core.publisher.Flux;
 import java.time.Duration;
 import java.util.Comparator;
 
-public class FluxConcatWith {
+public class FluxMergeOrderedWith {
 
     @Test
-    public void toMergeUsingConcatWith() {
+    public void toMergeUsingMergeOrderedWith() {
         Flux<Double> flux1 = Flux.interval(Duration.ofSeconds(1))
                 .take(3)
                 .map(Double::valueOf);
         Flux<Double> flux2 = Flux.just(0.5, 1.5, 2.5, 3.5);
 
-        flux1.concatWith(flux2)
+        Comparator<Double> comparator = (x, y) -> x.compareTo(y);
+        flux1.mergeOrderedWith(flux2, comparator)
                 .doOnNext(System.out::println)
                 .blockLast();
     }
