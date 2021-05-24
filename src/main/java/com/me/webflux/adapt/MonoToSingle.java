4310dae08b97eea1e2aedf10d835bd53d4ca403e
jaychy-yy <0816jinjin@gmail.com>
2021-05-24 21:32:07 +0900

    test: Mono가 Single로 변환되는지 테스트하였다.

--- src/main/java/com/me/webflux/adapt/MonoToSingle.java
+++ src/main/java/com/me/webflux/adapt/MonoToSingle.java
@@ -1,2 +1,10 @@
-package com.me.webflux.adapt;public class MonoToSingle {
+package com.me.webflux.adapt;
+
+import io.reactivex.rxjava3.core.Single;
+import reactor.core.publisher.Mono;
+
+public class MonoToSingle {
+    public static void main(String[] args) {
+        Single.fromPublisher(Mono.just("apple"));
+    }
 }
