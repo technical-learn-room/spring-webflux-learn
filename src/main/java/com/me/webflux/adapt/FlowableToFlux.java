813f03bffd26cc1868425d1f21f1196c76115221
jaychy-yy <0816jinjin@gmail.com>
2021-05-24 20:18:26 +0900

    test: Flowable을 Flux로 변환 테스트.

--- src/main/java/com/me/webflux/adapt/FlowableToFlux.java
+++ src/main/java/com/me/webflux/adapt/FlowableToFlux.java
@@ -1,2 +1,10 @@
-package com.me.webflux.adapt;public class FlowableToFlux {
+package com.me.webflux.adapt;
+
+import io.reactivex.rxjava3.core.Flowable;
+import reactor.core.publisher.Flux;
+
+public class FlowableToFlux {
+    public static void main(String[] args) {
+        Flux.from(Flowable.just("apple", "banana", "grape"));
+    }
 }
