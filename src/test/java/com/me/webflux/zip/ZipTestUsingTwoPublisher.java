47ee786be99c9998457ffb94dc8ec8e15f7601bb
jaychy-yy <0816jinjin@gmail.com>
2021-05-24 22:27:52 +0900

    test: zip() 메소드를 이용하여 Publisher 압축 테스트.

--- src/test/java/com/me/webflux/zip/ZipTestUsingTwoPublisher.java
+++ src/test/java/com/me/webflux/zip/ZipTestUsingTwoPublisher.java
@@ -1,2 +1,37 @@
-package com.me.webflux.zip;public class ZipTestUsingTwoPublisher {
+package com.me.webflux.zip;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+
+public class ZipTestUsingTwoPublisher {
+
+    @Test
+    public void zipTestUsingTwoPublisher() {
+        Flux<Integer> flux1 = Flux.just(1, 2, 3);
+        Flux<Integer> flux2 = Flux.just(4, 5, 6);
+
+        Flux.zip(flux1, flux2, (f1, f2) -> f1 * f2)
+                .doOnNext(System.out::println)
+                .subscribe();
+    }
+
+    @Test
+    public void zipTestUsingTwoPublisherAndDifferentType() {
+        Flux<Integer> flux1 = Flux.just(1, 2, 3);
+        Flux<String> flux2 = Flux.just("4", "5", "6");
+
+        Flux.zip(flux1, flux2, (f1, f2) -> f1.toString() + f2)
+                .doOnNext(System.out::println)
+                .subscribe();
+    }
+
+    @Test
+    public void zipTestUsingTwoPublisherAndDifferentArgumentCount() {
+        Flux<Integer> flux1 = Flux.just(1, 2, 3);
+        Flux<Integer> flux2 = Flux.just(4, 5);
+
+        Flux.zip(flux1, flux2, (f1, f2) -> f1 * f2)
+                .doOnNext(System.out::println)
+                .subscribe();
+    }
 }
