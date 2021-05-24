7d424981a321afa97b58a970612b0b432807663a
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-05-24 14:51:10 +0900

    test: onErrorContinue() 실행 후 mergeWith()를 통해 추가 데이터도 기존 스트림에 포함되는지 테스트하였다.

--- src/test/java/com/me/webflux/error/ErrorHandlingOnErrorContinue.java
+++ src/test/java/com/me/webflux/error/ErrorHandlingOnErrorContinue.java
@@ -2,20 +2,20 @@ package com.me.webflux.error;
 
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 public class ErrorHandlingOnErrorContinue {
 
     @Test
     public void onErrorContinueTest() {
         Flux.just("before data")
-                .mergeWith(Mono.error(RuntimeException::new))
-                .mergeWith(Mono.just("after data"))
+                .mergeWith(Flux.error(RuntimeException::new))
+                .mergeWith(Flux.just("after data"))
                 .log()
                 .onErrorContinue((e, o) -> {
                     System.out.println(String.format("error: %s", e));
                     System.out.println(String.format("object: %s", o));
                 })
+                .mergeWith(Flux.just("after after data"))
                 .doOnNext(System.out::println)
                 .subscribe();
     }
