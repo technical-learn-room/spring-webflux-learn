f32613b3834d2877f8dfaecaeae7ebd6db1b95cb
jaychy-yy <0816jinjin@gmail.com>
2021-05-24 21:27:10 +0900

    test: Observable을 Flux로 변환하는 테스트

--- src/main/java/com/me/webflux/adapt/ObservableToFlux.java
+++ src/main/java/com/me/webflux/adapt/ObservableToFlux.java
@@ -1,2 +1,11 @@
-package com.me.webflux.adapt;public class ObservableToFlux {
+package com.me.webflux.adapt;
+
+import io.reactivex.rxjava3.core.BackpressureStrategy;
+import io.reactivex.rxjava3.core.Observable;
+import reactor.core.publisher.Flux;
+
+public class ObservableToFlux {
+    public static void main(String[] args) {
+        Flux.from(Observable.just("apple", "banana", "grape").toFlowable(BackpressureStrategy.BUFFER));
+    }
 }
