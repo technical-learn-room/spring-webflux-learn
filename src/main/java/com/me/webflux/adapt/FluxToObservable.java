8f6b302b2e4686f3e4c2e4765264fed1cb32d201
jaychy-yy <0816jinjin@gmail.com>
2021-05-24 20:21:19 +0900

    test: Flux를 Observable로 변환 테스트.

--- src/main/java/com/me/webflux/adapt/FluxToObservable.java
+++ src/main/java/com/me/webflux/adapt/FluxToObservable.java
@@ -1,2 +1,10 @@
-package com.me.webflux.adapt;public class FluxToObservable {
+package com.me.webflux.adapt;
+
+import io.reactivex.rxjava3.core.Observable;
+import reactor.core.publisher.Flux;
+
+public class FluxToObservable {
+    public static void main(String[] args) {
+        Observable.fromPublisher(Flux.just("apple", "banana", "grape"));
+    }
 }
