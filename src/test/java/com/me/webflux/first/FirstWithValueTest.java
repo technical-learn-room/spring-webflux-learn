78579ce26a22c30d19a97520d94be9d971b63ee2
jaychy-yy <0816jinjin@gmail.com>
2021-05-25 09:35:51 +0900

    test: firstWithValue 테스트하기.

--- src/test/java/com/me/webflux/first/FirstWithValueTest.java
+++ src/test/java/com/me/webflux/first/FirstWithValueTest.java
@@ -1,2 +1,30 @@
-package com.me.webflux.first;public class FirstWithValueTest {
+package com.me.webflux.first;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
+public class FirstWithValueTest {
+
+    @Test
+    public void firstWithValueTest() {
+        Flux<Long> flux1 = Flux.interval(Duration.ofSeconds(5)).take(1);
+        Flux<Long> flux2 = Flux.interval(Duration.ofSeconds(3)).take(2);
+
+        Flux.firstWithValue(flux1, flux2)
+                .doOnNext(System.out::println)
+                .blockLast();
+    }
+
+    @Test
+    public void firstWithValueTestThrowError() {
+        Flux<Long> flux1 = Flux.interval(Duration.ofSeconds(5)).take(1);
+        Flux<Object> flux2 = Flux.error(RuntimeException::new)
+                .mergeWith(Flux.interval(Duration.ofSeconds(3)).take(2));
+
+        Flux.firstWithValue(flux1, flux2)
+                .doOnNext(System.out::println)
+                .blockLast();
+    }
 }
