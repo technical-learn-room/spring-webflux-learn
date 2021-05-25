479f938d50ce891a452c78c91878956d891f5b04
jaychy-yy <0816jinjin@gmail.com>
2021-05-25 15:44:53 +0900

    test: Flux.then() 메소드를 테스트함.

--- src/test/java/com/me/webflux/then/ThenTest.java
+++ src/test/java/com/me/webflux/then/ThenTest.java
@@ -1,2 +1,42 @@
-package com.me.webflux.then;public class ThenTest {
+package com.me.webflux.then;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public class ThenTest {
+
+    @Test
+    public void noArgumentThenTestNoError() {
+        Flux.just("apple", "banana", "grape")
+                .then()
+                .doOnNext(System.out::println)
+                .subscribe();
+    }
+
+    @Test
+    public void noArgumentThenTestThrowError() {
+        Flux.just("apple", "banana", "grape")
+                .mergeWith(Mono.error(RuntimeException::new))
+                .then()
+                .doOnNext(System.out::println)
+                .subscribe();
+    }
+
+    @Test
+    public void monoArgumentThenTestNoError() {
+        Flux.just("apple", "banana", "grape")
+                .then(Mono.just("pineapple"))
+                .doOnNext(System.out::println)
+                .subscribe();
+    }
+
+    @Test
+    public void monoArgumentThenTestThrowError() {
+        Flux.just("apple", "banana", "grape")
+                .mergeWith(Mono.error(RuntimeException::new))
+                .then(Mono.just("pineapple"))
+                .doOnNext(System.out::println)
+                .subscribe();
+    }
 }
