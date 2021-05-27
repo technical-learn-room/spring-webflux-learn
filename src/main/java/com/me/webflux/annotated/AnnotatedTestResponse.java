c69f0dce2bd716ba7fc0de1f1dbbd4f9d9ffa902
jaychy-yy <0816jinjin@gmail.com>
2021-05-27 21:28:47 +0900

    feat: 테스트용 Response DTO 생성.

--- src/main/java/com/me/webflux/annotated/AnnotatedTestResponse.java
+++ src/main/java/com/me/webflux/annotated/AnnotatedTestResponse.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
-public class TestResponse {
+public class AnnotatedTestResponse {
     private String name;
     private int age;
     private String gradeClassNumber;
