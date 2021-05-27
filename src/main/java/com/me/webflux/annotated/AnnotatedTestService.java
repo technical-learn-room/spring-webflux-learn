84496de0ac59573995951af0b0d27c2d05a8f3c0
jaychy-yy <0816jinjin@gmail.com>
2021-05-27 21:29:36 +0900

    feat: firstName과 lastName을 합쳐주는 메소드와 학년, 반, 번호를 합쳐주는 메소드 생성.

--- src/main/java/com/me/webflux/annotated/AnnotatedTestService.java
+++ src/main/java/com/me/webflux/annotated/AnnotatedTestService.java
@@ -1,2 +1,15 @@
-package com.me.webflux.annotated;public class AnnotatedTestService {
+package com.me.webflux.annotated;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AnnotatedTestService {
+
+    public String combineName(String firstName, String lastName) {
+        return lastName + firstName;
+    }
+
+    public String combineGradeClassNumber(int grade, int classroom, int number) {
+        return String.format("%d%d%d", grade, classroom, number);
+    }
 }
